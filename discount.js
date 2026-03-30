279805c2d7720da85cb53975ee68d2e776a16c37
12keza <igizenezakezab@gmail.com>
2026-03-30 10:16:51 -0700

    the file discound was already apdated

---
+++ discount.js
@@ -0,0 +1,28 @@
+function discount (){
+let iterm1 = 500;
+let iterm2 = 1000;
+let iterm3 = 3000;
+let iterm4 = 20000;
+
+if(iterm1>=600){
+    console.log("discount applied")
+}else{
+    console.log("discount dineied")
+}
+if(iterm2>=600){
+    console.log("discount applied")
+}else{
+    console.log("discount denied")
+}
+if(iterm3>=600){
+    console.log("discount applied")
+}else{
+    console.log("discount denied")
+}
+if(iterm4>=600){
+    console.log("discound applied")
+}else{
+    console.log("discount denied")
+}
+}
+discount()
