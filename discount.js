1c7cf354ac268fdb936e93b1e61fad7141bee407
12keza <igizenezakezab@gmail.com>
2026-03-30 22:18:53 -0700

    discount added

--- discount.js
+++ discount.js
@@ -4,25 +4,41 @@ let iterm2 = 1000;
 let iterm3 = 3000;
 let iterm4 = 20000;
 
-if(iterm1>=600){
-    console.log("discount applied")
-}else{
-    console.log("discount dineied")
+
+     if (item1 > 600) {
+        let disc = item1 * 0.5 / 100;
+        let finalPrice = item1 - disc;
+        console.log(item1 + " -> discount applied, final price: " + finalPrice);
+    } else {
+        console.log(item1 + " -> discount denied");
+    }
+    if(iterm2 > 600){
+        let  disc = iterm2 * 0.5/100;
+        let finalPrice = iterm2-disc;
+        console.log(iterm2 + "-> discount applied, final price:+ finalprice");
+
+    }
+    if (item3> 600) {
+        let disc = item1 * 0.5 / 100;
+        let finalPrice = item1 - disc;
+        console.log(item2+ " -> discount applied, final price: " + finalPrice);
+    } else {
+        console.log(item2 + " -> discount denied");
+    }
+    if (item3 > 600) {
+        let disc = item1 * 0.5 / 100;
+        let finalPrice = item1 - disc;
+        console.log(item3+ " -> discount applied, final price: " + finalPrice);
+    } else {
+        console.log(item3 + " -> discount denied");
+    }
+    if (item4 > 600) {
+        let disc = item1 * 0.5 / 100;
+        let finalPrice = item1 - disc;
+        console.log(item4 + " -> discount applied, final price: " + finalPrice);
+    } else {
+        console.log(item4 + " -> discount denied");
+    }
 }
-if(iterm2>=600){
-    console.log("discount applied")
-}else{
-    console.log("discount denied")
-}
-if(iterm3>=600){
-    console.log("discount applied")
-}else{
-    console.log("discount denied")
-}
-if(iterm4>=600){
-    console.log("discound applied")
-}else{
-    console.log("discount denied")
-}
-}
+
 discount()
