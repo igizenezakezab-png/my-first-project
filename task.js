8d2fe8cd3088b6ea39d11e1d2a507c5c539339ae
12keza <igizenezakezab@gmail.com>
2026-03-30 19:01:57 -0700

    loop of discount

--- task.js
+++ task.js
@@ -1,7 +1,15 @@
-function sum(num1,num2){
-    const Sum =num1+num2;
-    return sum;
-};
 
+//loop 
+function discount(){
+    let iterms = [500,1000,3000,20000];
+    for(let i = 0; i < iterms.length;i++){
+        
+        if (iterms[1] > 600){
+            console.log(iterm[i] + "discount applied");
 
-//gdsffsahf
+        }else{
+            console.log(iterms[i] + "discount denied");
+            
+        }
+    }
+}
