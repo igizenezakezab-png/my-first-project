8767b8353fd884755ada67297aee14e08cda8b65
12keza <igizenezakezab@gmail.com>
2026-03-30 23:38:49 -0700

    task added

--- lps.js
+++ lps.js
@@ -1,9 +1,23 @@
+function discount() {
 
-let i = 0;
+    let items = [500, 1000, 3000, 20000];
 
-while(i<=10);{
-    text  +="the number is" + i;
-    console.log(i);
+    
+    for (let i = 0; i < items.length; i++) {
 
-    i++;
+
+        let price = items[i];
+
+    
+        if (price > 600) {
+            
+            console.log("final price:", price - (price * 0.5 / 100));
+        } else {
+            
+            console.log("discount denied");
+        }
+    }
 }
+
+
+discount();
