921e1f77472242b9bfc548456a6696d3d578304e
12keza <igizenezakezab@gmail.com>
2026-04-01 21:56:54 -0700

    work done

--- function.js
+++ function.js
@@ -1,22 +1,42 @@
-//enterclub
+
 function enter_club(password) {
     if (password === "Apple123") {
-        return "Access Granted"; 
+        return "Access Granted";
     } else {
         return "Access Denied";
     }
 }
-let users = [
-    { name: "Alice", password: "Apple123" },
-    { name: "Bob", password: "wrong1" },
-    { name: "John", password: "Apple123" },
-    { name: "Emma", password: "12345" }
-];
-for (let i = 0; i < users.length; i++) {
-      if (users[i].password === "Apple123") {
-        console.log(users[i].name + ": Access Granted");
-    } else {
-        console.log(users[i].name + ": Access Denied");
-    }
+
+
+let guest1 = "app0";
+let guest2 = "Apple123";
+let guest3 = "hello";
+let guest4 = "Apple123";
+
+// Guest1
+if (guest1 === "Apple123") {
+    console.log("Guest1: Access Granted");
+} else {
+    console.log("Guest1: Access Denied");
 }
 
+// Guest2
+if (guest2 === "Apple123") {
+    console.log("Guest2: Access Granted");
+} else {
+    console.log("Guest2: Access Denied");
+}
+
+// Guest3
+if (guest3 === "Apple123") {
+    console.log("Guest3: Access Granted");
+} else {
+    console.log("Guest3: Access Denied");
+}
+
+// Guest4
+if (guest4 === "Apple123") {
+    console.log("Guest4: Access Granted");
+} else {
+    console.log("Guest4: Access Denied");
+}
