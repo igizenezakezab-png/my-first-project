1484a6c19d089023a9d48c5bea0688583c1b4094
12keza <igizenezakezab@gmail.com>
2026-04-01 18:50:17 -0700

    function added

--- function.js
+++ function.js
@@ -1,6 +1,17 @@
-function alertRandom() {
-    const randomNumber = Math.floor(Math.random() * 6) + 1;
-    console.log(randomNumber);
+//enter club
+function enter_club(password) {
+    if (password === "Apple123") {
+        return "Access Granted";
+    } else {
+        return "Access Denied"; 
+    }
 }
 
-alertRandom();
+//check the weather
+function check_weather(temperature) {
+    if (temperature > 25) {
+        return "It is a hot day!";
+    } else {
+        return "It is a lovely day!";
+    }
+}
