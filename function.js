df59e116f6e810f0efb646aa67e5c0ed701b0586
12keza <igizenezakezab@gmail.com>
2026-04-01 21:29:13 -0700

    function added

--- function.js
+++ function.js
@@ -1,13 +1,26 @@
-//enter club
+//enterclub
 function enter_club(password) {
     if (password === "Apple123") {
-        return "Access Granted";
+        return "Access Granted"; 
     } else {
-        return "Access Denied"; 
+        return "Access Denied";
+    }
+}
+let users = [
+    { name: "Alice", password: "Apple123" },
+    { name: "Bob", password: "wrong1" },
+    { name: "John", password: "Apple123" },
+    { name: "Emma", password: "12345" }
+];
+for (let i = 0; i < users.length; i++) {
+      if (users[i].password === "Apple123") {
+        console.log(users[i].name + ": Access Granted");
+    } else {
+        console.log(users[i].name + ": Access Denied");
     }
 }
 
-//check the weather
+let weather=0;
 function check_weather(temperature) {
     if (temperature > 25) {
         return "It is a hot day!";
@@ -15,3 +28,6 @@ function check_weather(temperature) {
         return "It is a lovely day!";
     }
 }
+let answer=check_weather(temperature);
+console.log("your temperature:",temperature);
+console.log("answer:",answer);
